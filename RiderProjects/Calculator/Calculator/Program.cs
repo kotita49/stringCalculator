46629ddcce08ca96e3bbc6d740a053f5c31c8336
tatyana rutkowska <kotita49@hotmail.com>
2020-12-09 12:00:21 +0000

    Added simple multiplier

---
+++ RiderProjects/Calculator/Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator!");
+            Console.Write("Enter first number");
+           string first = Console.ReadLine();
+           int firstInt = int.Parse(first);
+           Console.WriteLine(firstInt);
+           Console.Write("Enter second number");
+           string second = Console.ReadLine();
+           int secondInt = int.Parse(second);
+           Console.WriteLine(secondInt);
+           int multiplier = firstInt * secondInt;
+           Console.WriteLine("The answer is :" + multiplier);
+        }
+    }
+}
